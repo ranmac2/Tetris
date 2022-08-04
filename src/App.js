a4346f69d7f6202e9bcc61668eabc27845c65375
Randy MacLean <randy_maclean@hotmail.ca>
2022-08-04 16:09:58 -0700

    created Game.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Game from "./components/Game";
 
 function App() {
   return (
     <div className="app">
       <div className="app-header">Tetris</div>
+      <Game rows={20} columns={10} />
     </div>
   );
 }
---
+++ src/components/Game.js
@@ -0,0 +1,7 @@
+import react, { Component } from "react";
+
+const Game = ({ rows, columns }) => {
+  return <p>game</p>;
+};
+
+export default Game;
