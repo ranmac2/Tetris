80e34ff6ef3e93e4812ea62a98c5dae61bf9cc23
Randy MacLean <randy_maclean@hotmail.ca>
2022-08-08 13:26:58 -0700

    added BoardCell component and BoardCell.css

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -4,7 +4,8 @@ import Board from "./Board";
 import { useBoard } from "../hooks/useBoard";
 
 const Tetris = ({ rows, columns, setGameOver }) => {
-  return <Board />;
+  const [board, setBoard] = useBoard({ rows, columns });
+  return <Board board={board} />;
 };
 
 export default Tetris;
--- src/hooks/useBoard.js
+++ src/hooks/useBoard.js
@@ -1,9 +1,7 @@
 import { useState } from "react";
 import buildBoard from "../business/Board";
 
-const useBoard = ({ rows, columns }) => {
+export const useBoard = ({ rows, columns }) => {
   const [board, setBoard] = useState(buildBoard({ rows, columns }));
   return [board];
 };
-
-export default useBoard;
